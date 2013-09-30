4f5dcb8f2f82b6034fafd7156dec1e808b4f06e3
akazorin <akazorin@gmail.com>
2013-09-30 17:56:46 +0300

    Init

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'passwd' => '123'
+);
---
+++ hook.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('inc.php');
+header('Content-Type: text/plain');
+if (file_exists('key.php'))
+{
+    $key = require('key.php');
+    if ($key['current'] === $_SERVER['QUERY_STRING'])
+    {
+        $submodule = getcwd();
+        $project = $submodule.'/../../../';
+        chdir(dirname($project));
+        _exec('pwd');
+        _exec('git pull');
+        _exec('git submodule update');
+    }
+}
+
+_log(var_export($_POST, true));
